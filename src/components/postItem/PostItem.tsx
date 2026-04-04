190ccf13f8e1c9aaa38cd836180242ef37d5bcaa
seyar_ad <ad-abzatov.seyar@mail.ru>
2026-04-05 00:13:48 +0300

    add sub

--- src/components/postItem/PostItem.tsx
+++ src/components/postItem/PostItem.tsx
@@ -1,14 +1,27 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 
 interface PostItemProps {
+  id: number;
   title: string;
+  addSub: (postId: number, title: string) => Promise<void>;
   subPosts?: {
     id: number;
     title: string;
   }[];
 }
 
-const PostItem: FC<PostItemProps> = ({title, subPosts}) => {
+const PostItem: FC<PostItemProps> = ({id, title, subPosts, addSub}) => {
+
+  const [subTitle, setSubTitle] = useState('');
+
+  const handleAddSub = async (e: React.FormEvent) => {
+    e.preventDefault();
+    if (!subTitle.trim()) return;
+
+    await addSub(id, title);
+    setSubTitle('');
+  }
+
   return (
     <div>
       <div>
@@ -16,6 +29,9 @@ const PostItem: FC<PostItemProps> = ({title, subPosts}) => {
       </div>
       <div>
         {subPosts?.map(subPost => <div key={subPost.id}>{subPost.title}</div>)}
+      </div>
+      <div>
+        <button onClick={handleAddSub}>Новая подзадача</button>
       </div>
     </div>
   )
